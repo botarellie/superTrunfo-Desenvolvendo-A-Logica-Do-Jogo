fb4e649cc1fdf3021f2765b6dda3eff96945cfef
botarellie <100967410+botarellie@users.noreply.github.com>
2025-09-24 02:02:28 +0000

    LogicaDoJogov3

--- superTrunfo-Desenvolvendo-A-Logica-Do-Jogo.c
+++ superTrunfo-Desenvolvendo-A-Logica-Do-Jogo.c
@@ -56,11 +56,11 @@ int main() {
     // // printf("\nDigite um Estado(UF): ");
     // // scanf(" %49[^\n]", estado1);
      
-    // printf("\nDigite o nome do país: ");
-    // scanf(" %49[^\n]", nomePais1);   
+    printf("\nDigite o nome do país: ");
+    scanf(" %49[^\n]", nomePais1);   
 
-    // printf("\nDigite a população do país: ");
-    // scanf("%lu", &populacao1);  
+    printf("\nDigite a população do país: ");
+    scanf("%lu", &populacao1);  
 
     // printf("\nDigite a área do país: ");
     // scanf(" %f", &area1);
@@ -71,8 +71,8 @@ int main() {
     // printf("\nDigite o número de pontos turísticos do país: ");
     // scanf(" %d", &numPontosTuristicos1);
 
-    // //Calcula densidade populacional da Pais
-    // densidadePopulacional1 = (float)populacao1 / area1;
+    //Calcula densidade populacional da Pais
+    densidadePopulacional1 = (float)populacao1 / area1;
 
     // //Calcula Pib per Capta da Pais
     // pibPerCapta1 = ((float)populacao1 / pib1);
@@ -90,11 +90,11 @@ int main() {
     // // printf("\nDigite uma Estado(UF): ");
     // // scanf(" %49[^\n]", estado2);
 
-    // printf("\nDigite o nome do país: ");
-    // scanf(" %49[^\n]", nomePais2);   
+    printf("\nDigite o nome do país: ");
+    scanf(" %49[^\n]", nomePais2);   
 
-    // printf("\nDigite a população do país: ");
-    // scanf("%lu", &populacao2);  
+    printf("\nDigite a população do país: ");
+    scanf("%lu", &populacao2);  
 
     // printf("\nDigite a área do país: ");
     // scanf(" %f", &area2);
@@ -105,8 +105,8 @@ int main() {
     // printf("\nDigite o número de pontos turísticos do país:: ");
     // scanf(" %d", &numPontosTuristicos2);
 
-    // //Calcula densidade populacional da Pais
-    // densidadePopulacional2 = (float)populacao2 / area2;
+    //Calcula densidade populacional da Pais
+    densidadePopulacional2 = (float)populacao2 / area2;
 
     // //Calcula Pib per Capta da Pais
     // pibPerCapta2 = ((float)populacao2 / pib2);
@@ -167,10 +167,58 @@ int main() {
         break;
     }
     
+    printf("\nOs países comparados são: %s vs %s\n", nomePais1, nomePais2);
+
+    //Exibe atributos país 1
+    printf("\nAtributos %s", nomePais1);
+    printf("\n---------------------------------------------------------\n");
+
+    printf("\nCarta %s%s: %s", codPais1,numCodPais1,nomePais1);
+    printf("\nPopulação do país: %lu", populacao1);                                   // Exibe a população do país
+    printf("\nÁrea do país: %.2f", area1);                                            // Exibe a área do país
+    printf("\nPIB do país: %.2f", pib1);                                              // Exibe o PIB do país
+    printf("\nNúmero de pontos turísticos do país: %d", numPontosTuristicos1);        // Exibe o numero de pontos turísticos do país
+    printf("\nDensidade Populacional: %.2f hab/km²", densidadePopulacional1);         // Exibe a quantidade de habitantes por km²
+    printf("\nPIB per Capita: %.2f reais\n", pibPerCapta1);                           // Exibe o PIB per capita do país
+    printf("\nDensidade demográfica: %.2f", densidadePopulacional1);                       // Exibe a Densidade demográfica
+
+    printf("\n---------------------------------------------------------\n");
+
+    //Exibe atributos país 2
+    printf("\nAtributos %s", nomePais2);
+    printf("\n---------------------------------------------------------\n");
+    printf("\nCarta %s%s: %s", codPais2,numCodPais2,nomePais2);
+    printf("\nPopulação do país: %lu", populacao2);                                   // Exibe a população do país
+    printf("\nÁrea do país: %.2f", area2);                                            // Exibe a área do país
+    printf("\nPIB do país: %.2f", pib2);                                              // Exibe o PIB do país
+    printf("\nNúmero de pontos turísticos do país: %d", numPontosTuristicos2);        // Exibe o numero de pontos turísticos do país
+    printf("\nDensidade Populacional: %.2f hab/km²", densidadePopulacional2);         // Exibe a quantidade de habitantes por km²
+    printf("\nPIB per Capita: %.2f reais\n", pibPerCapta2);                           // Exibe o PIB per capita do país
+    printf("\nDensidade demográfica: %.2f", densidadePopulacional2);                    // Exibe a Densidade demográfica
+
+    printf("\n---------------------------------------------------------\n");
+
     printf("\nAtributo escolhido foi: %s", atributoDeComparacao);
+
     
-    
-    
+    //Compara atributo escolhido
+    if (atributoEscolhido == 1)
+       printf("\nif atributo escolhido =1\n");
+    {
+        if (populacao1 > populacao2)
+        {
+            printf("\nif população 1 >\n");
+            printf("\nA carta do país %s venceu!", nomePais1);
+        }
+        else if(populacao1 < populacao2){
+                        printf("\nif população 2 >\n");
+                printf("\nA carta do país %s venceu!", nomePais2);
+            }
+            else{
+                printf("\nEmpatou!");
+            };
+    }
+
 
     printf("\nFIM\n");
     
